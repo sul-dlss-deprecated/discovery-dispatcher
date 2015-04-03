b6bea838f3ef68616ba7ae19321b5a344c50e005
Ahmed AlSum <aalsum@stanford.edu>
2015-04-03 09:55:02 -0700

    Refactor the classes location

--- lib/discovery_dispatcher/purl_fetcher_manager.rb
+++ lib/discovery_dispatcher/purl_fetcher_manager.rb
@@ -6,11 +6,11 @@ module DiscoveryDispatcher
     PACIFIC_TIME_ZONE = 'Pacific Time (US & Canada)'
     def self.get_next_start_time
       next_start_time = ReaderLogRecords.maximum(:last_read_time) 
-      return next_start_time.nil? ? Time.parse("1970-01-01T12:00:00").in_time_zone(PACIFIC_TIME_ZONE) : next_start_time.in_time_zone(PACIFIC_TIME_ZONE)
+      return next_start_time.nil? ? Time.parse("1970-01-01T12:00:00").in_time_zone(PACIFIC_TIME_ZONE).iso8601 : next_start_time.in_time_zone(PACIFIC_TIME_ZONE).iso8601
     end
     
     def self.set_last_fetch_info(last_read_time, no_of_records)
-      ReaderLogRecords.create(last_read_time: Time.parse(last_read_time).utc, record_count: no_of_records)
+      ReaderLogRecords.create(last_read_time: Time.parse(last_read_time).in_time_zone(PACIFIC_TIME_ZONE), record_count: no_of_records)
     end
   end
 end
--- spec/discovery_dispatcher/purl_fetcher_manager_spec.rb
+++ spec/discovery_dispatcher/purl_fetcher_manager_spec.rb
@@ -1,19 +1,45 @@
 describe DiscoveryDispatcher::PurlFetcherManager do
   
   describe ".get_next_start_time" do
-    before :all do
+    before :each do
+      ReaderLogRecords.delete_all
     end
     
     it "should return the next start time" do
-      ReaderLogRecords.create(last_read_time: "2010-01-01T12:00:00",record_count: 1)
+      ReaderLogRecords.create(last_read_time: "2010-01-01T12:00:00 -0800",record_count: 1)
+      ReaderLogRecords.create(last_read_time: "2011-01-01T12:00:00 -0800",record_count: 1)
+      ReaderLogRecords.create(last_read_time: "2012-01-01T12:00:00 -0800",record_count: 1)
+      
       start_time = DiscoveryDispatcher::PurlFetcherManager.get_next_start_time
-      puts start_time.in_time_zone('Eastern Time (US & Canada)')
+      expect(start_time).to eq("2012-01-01T12:00:00-08:00")
+    end
+
+    it "should return 1970 as the start time for empty table" do
+      start_time = DiscoveryDispatcher::PurlFetcherManager.get_next_start_time
+      expect(start_time).to eq("1970-01-01T12:00:00-08:00")
+    end
+    
+    after :each do
       ReaderLogRecords.delete_all
     end
+    
   end
   
   describe ".set_last_fetch_info" do
-    pending
+    before :each do
+      ReaderLogRecords.delete_all
+    end
+    
+    it "should insert the last time and the count" do
+      DiscoveryDispatcher::PurlFetcherManager.set_last_fetch_info Time.parse("2015-01-01T12:00:00 -0800").to_s , 5
+      last_id = ReaderLogRecords.maximum(:id)
+      record = ReaderLogRecords.find last_id
+      expect(record.last_read_time).to eq(Time.parse("2015-01-01T12:00:00 -0800"))
+      expect(record.record_count).to eq(5)
+    end
+    after :each do
+      ReaderLogRecords.delete_all
+    end
   end
   
 end
