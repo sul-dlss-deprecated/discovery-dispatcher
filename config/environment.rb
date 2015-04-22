cdb328072cdc9b51e35c5729ab96bb4df329f8b6
Ahmed AlSum <aalsum@stanford.edu>
2015-04-22 12:02:22 -0700

    Change the logger format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.logger.formatter = proc do |severity, datetime, progname, msg|
+    date_format = datetime.strftime("%Y-%m-%d %H:%M:%S")
+    "[#{date_format}] #{severity} (#{progname}): #{msg}\n"
+end
