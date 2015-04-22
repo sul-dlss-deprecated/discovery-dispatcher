08f99dbd40830212522254c9d69b62329236e7f2
Ahmed AlSum <aalsum@stanford.edu>
2015-04-22 12:01:22 -0700

    Indexing job reports any error to delayed queue

--- lib/discovery_dispatcher/indexing_job.rb
+++ lib/discovery_dispatcher/indexing_job.rb
@@ -1,20 +1,28 @@
 module DiscoveryDispatcher
   class IndexingJob < Struct.new(:type, :druid_id, :target, :subtargets)
     def perform
-      Rails.logger.info "Processing #{druid_id} for target #{target}"
+      Rails.logger.debug "Processing #{druid_id} for target #{target}"
       druid = druid_id.gsub("druid:","")  
       target_url  = get_target_url target, druid
       method      = get_method type, druid   
       subtarets_param = get_subtarets_param subtargets
     
       request_command = "RestClient.#{method} \"#{target_url}/items/#{druid}#{subtarets_param}\", \"\""
-      puts request_command
+      response = nil
       begin
-        eval(request_command)
-      rescue => e
+        response = eval(request_command)
+      rescue => e # An exception happened during the request
         raise "#{type} #{druid} with #{request_command} has an error.\n#{e.inspect}\n#{e.message}\n#{e.backtrace}"
       end
-      Rails.logger.info "Completing #{druid_id} for target #{target}"
+      if response.nil? then # The request doesn't raise an exception but it doesn't return data
+        raise "#{type} #{druid} with #{request_command} has an error."
+      end
+      if response.code != 200 then # The request return with anything other than 200, so it is a problem
+        raise "#{type} #{druid} with #{request_command} has an error.\n#{response.code}\n\n#{response.inspect}"
+      end
+      
+      # Request went successfully
+      Rails.logger.debug "Completing #{druid_id} for target #{target}"
     end      
       
     def get_target_url target,druid
