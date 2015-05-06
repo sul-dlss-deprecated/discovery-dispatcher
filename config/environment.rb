44693a6385d0159aea49d167a973909be9d5c86f
Ahmed AlSum <aalsum@stanford.edu>
2015-05-06 14:35:24 -0700

    Change the logformatter

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,10 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Rails.logger.formatter = proc do |severity, datetime, progname, msg|
-    date_format = datetime.strftime("%Y-%m-%d %H:%M:%S")
+class SimpleFormatter < ::Logger::Formatter
+  # This method is invoked when a log event occurs
+  def call(severity, timestamp, progname, msg)
+    date_format = timestamp.strftime("%Y-%m-%d %H:%M:%S")
     "[#{date_format}] #{severity} (#{progname}): #{msg}\n"
+  end
 end
