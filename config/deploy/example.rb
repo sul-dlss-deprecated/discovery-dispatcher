b5316543b3591d0fd6c2cbddd2cba71a0e1346ab
Ahmed AlSum <aalsum@stanford.edu>
2015-05-12 11:27:16 -0700

    Add config/deploy/example.rb as deployment environment example

---
+++ config/deploy/example.rb
@@ -0,0 +1,7 @@
+server '', user: '', roles: %w{web app db}
+
+Capistrano::OneTimeKey.generate_one_time_key!
+
+set :deploy_environment, ''
+set :whenever_environment, fetch(:deploy_environment)
+set :rails_env, ''
