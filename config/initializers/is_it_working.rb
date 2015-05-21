380fbc56f64bfa45a6eab3294bb78379509d265e
Ahmed AlSum <aalsum@stanford.edu>
2015-05-21 11:42:07 -0700

    Update is_it_working with more checks

--- config/initializers/is_it_working.rb
+++ config/initializers/is_it_working.rb
@@ -1,3 +1,5 @@
 require 'is_it_working'
 Rails.configuration.middleware.use(IsItWorking::Handler) do |h|
+  h.check :active_record, :async => false
+  h.check :url, :get =>  "#{Rails.configuration.purl_fetcher_url}/is_it_working"
 end
