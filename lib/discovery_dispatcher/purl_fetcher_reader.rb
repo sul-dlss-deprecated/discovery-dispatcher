ca9f459494a0fe1c2f94646fa7926dfeb9cb4029
Laney McGlohon <laneymcg@stanford.edu>
2015-09-17 09:54:34 -0700

    Added tests for read_index_list and added processing of falses to the read_index_list method

--- lib/discovery_dispatcher/purl_fetcher_reader.rb
+++ lib/discovery_dispatcher/purl_fetcher_reader.rb
@@ -1,76 +1,86 @@
 require 'json'
 
 module DiscoveryDispatcher
-  
   # It reads the indexed/deleted records from purl-fetcher
   class PurlFetcherReader
-    
     # @param start_time [String] the start time for the period we need to read the records from
     # @param end_time [String] the end time for the period we need to read the records to
     def initialize(start_time, end_time)
       @start_time = start_time
       @end_time = end_time
     end
-    
+
     # It reads and merge both of the indexing and deleted records from purl-fetcher
     # @return a list of records sorted by last modified time
     def load_records
       begin
         index_records = read_index_list
-      rescue =>e
+      rescue => e
         message = "#{e.message}\n"
-        e.backtrace.each { |l| message = "#{message}#{l}\n"}
-        raise  DiscoveryDispatcher::Errors::MissingPurlFetcherIndexPage.new(message )
+        e.backtrace.each { |l| message = "#{message}#{l}\n" }
+        raise DiscoveryDispatcher::Errors::MissingPurlFetcherIndexPage.new(message)
       end
 
       begin
-        delete_records = read_delete_list       
-      rescue =>e
+        delete_records = read_delete_list
+      rescue => e
         message = "#{e.message}\n"
-        e.backtrace.each { |l| message = "#{message}#{l}\n"}
-        raise  DiscoveryDispatcher::Errors::MissingPurlFetcherDeletePage.new(message )
+        e.backtrace.each { |l| message = "#{message}#{l}\n" }
+        raise DiscoveryDispatcher::Errors::MissingPurlFetcherDeletePage.new(message)
       end
 
       all_records = merge_and_sort(index_records, delete_records)
     end
-    
+
     # @return [Hash] a hash of the indexed items between the start and end times, or {} if ther's no records
     def read_index_list
-      index_page =  RestClient.get "#{Rails.configuration.purl_fetcher_url}/docs/changes", {:params => {:first_modified => @start_time, :last_modified => @end_time, :content_type => :json, :accept => :json}}
-      if index_page.present? then
+      index_page =  RestClient.get "#{Rails.configuration.purl_fetcher_url}/docs/changes", params: { first_modified: @start_time, last_modified: @end_time, content_type: :json, accept: :json }
+      if index_page.present?
         return JSON.parse(index_page)
       else
         return {}
       end
     end
-    
+
     # @return [Hash] a hash of the deleted items between the start and end times, or {} if ther's no records
     def read_delete_list
-    #  delete_page = RestClient.get "#{Rails.configuration.purl_fetcher_url}/docs/deletes", {:params => {:first_modified => @start_time, :last_modified => @end_time, :content_type => :json, :accept => :json}}
+      #  delete_page = RestClient.get "#{Rails.configuration.purl_fetcher_url}/docs/deletes", {:params => {:first_modified => @start_time, :last_modified => @end_time, :content_type => :json, :accept => :json}}
       delete_page = nil
-      if delete_page.present? then
-        return JSON.parse(delete_page) 
+      if delete_page.present?
+        return JSON.parse(delete_page)
       else
         return {}
       end
     end
-    
+
     # It merges both indexed and deleted records in one list, and sort the new list by last_modified time
     # @return [Array] a list of sorted/merged indexed and deleted records.
     def merge_and_sort(index_records, delete_records)
-      all_records_list=[]
-      if not(index_records.nil?) and index_records.include?("changes") and not(index_records["changes"].empty?)
-        index_records["changes"].each do |index_record| 
-          all_records_list.push( index_record.update( {:type=>"index" }))
-        end 
+      all_records_list = []
+      if index_records && index_records['changes']
+        index_records['changes'].each do |index_record|
+          if index_record['true_targets']
+            index_record['true_targets'].each do |target|
+              all_records_list.push(druid: index_record['druid'], latest_change: index_record['latest_change'], target: target, type: 'index')
+            end
+          end
+          if index_record['false_targets']
+            index_record['false_targets'].each do |target|
+              all_records_list.push(druid: index_record['druid'], latest_change: index_record['latest_change'], target: target, type: 'delete')
+            end
+          end
+        end
       end
-      if not(delete_records.nil?) and delete_records.include?("deletes") and not(delete_records["deletes"].empty?)
-        delete_records["deletes"].each do |delete_record| 
-          all_records_list.push( delete_record.update( {:type=>"delete" }))
-        end 
-      end 
-      
-      return all_records_list.sort_by{ |item| item["latest_change"]}
+      if delete_records && delete_records['deletes']
+        delete_records['deletes'].each do |delete_record|
+          all_records_list.push(druid: delete_record['druid'], latest_change: delete_record['latest_change'], type: 'delete')
+        end
+      end
+
+      all_records_list.each do |thing|
+        puts "#{thing}"
+      end
+      all_records_list.sort_by { |item| item[:latest_change] } if all_records_list
     end
   end
 end
--- spec/discovery_dispatcher/purl_fetcher_reader_spec.rb
+++ spec/discovery_dispatcher/purl_fetcher_reader_spec.rb
@@ -1,58 +1,64 @@
 require 'json'
 
 describe DiscoveryDispatcher::PurlFetcherReader do
-  
-  describe ".load_records" do
-    pending
-  end
-  
-  describe ".read_index_list" do
+  describe '.load_records' do
     pending
   end
 
-  describe ".read_delete_list" do
+  describe '.read_index_list' do
+    it 'should return a JSON formatted hash' do
+      reader = DiscoveryDispatcher::PurlFetcherReader.new('2015-07-03T00:00:57.046Z', '2015-09-01T19:00:11.869Z')
+      index_page = '{"changes":[{"druid":"druid:bb298yx8728","latest_change":"2015-07-03T01:00:57.046Z","true_targets":["revs_stage"],"false_targets":["Atago","Robot_testing_feb_5_2015"]}, {"druid":"druid:bx498hg0161","latest_change":"2015-09-01T17:00:11.869Z","true_targets":["SearchWorks","sw_stage"],"false_targets":["Sw-stage"]}]}'
+      RestClient.stub(:get).and_return(index_page)
+      expect(reader.read_index_list).to eq(JSON.parse(index_page))
+    end
+  end
+
+  describe '.read_delete_list' do
     pending
   end
-  
-  describe ".merge_and_sort" do
-    it "should return a sorted/merged list for index and delete" do
-      reader = DiscoveryDispatcher::PurlFetcherReader.new("","")
-      
-      index_hash= '{"changes":[ {"druid":"druid:aa111aa1111","latest_change":"2014-12-20T21:03:32Z","targets":["Searchwork","Revs"]},  {"druid":"druid:bb111bb1111","latest_change":"2014-12-15T10:03:32Z","targets":["Revs"]}]}'
+
+  describe '.merge_and_sort' do
+    it 'should return a sorted/merged list for index and delete' do
+      reader = DiscoveryDispatcher::PurlFetcherReader.new('', '')
+
+      index_hash = '{"changes":[ {"druid":"druid:aa111aa1111","latest_change":"2014-12-20T21:03:32Z","true_targets":["Searchwork","Revs"],"false_targets":["Sw_stage"]},  {"druid":"druid:bb111bb1111","latest_change":"2014-12-15T10:03:32Z","true_targets":["Revs"]}]}'
       delete_hash = '{"deletes":[{"druid":"druid:aa111aa1111","latest_change":"2014-12-15T21:03:32Z"},{"druid":"druid:bb111bb1111","latest_change":"2014-12-10T21:03:32Z"} ]}'
-      merged_hash = reader.merge_and_sort(JSON.parse(index_hash), JSON.parse(delete_hash) )
-      
-      expect(merged_hash.length).to eq(4)
-      expect(merged_hash[0]).to eq({"druid"=>"druid:bb111bb1111", "latest_change"=>"2014-12-10T21:03:32Z", :type=>"delete"})
-      expect(merged_hash[1]).to eq({"druid"=>"druid:bb111bb1111", "latest_change"=>"2014-12-15T10:03:32Z", "targets"=>["Revs"], :type=>"index"} )
-      expect(merged_hash[2]).to eq({"druid"=>"druid:aa111aa1111", "latest_change"=>"2014-12-15T21:03:32Z", :type=>"delete"} )
-      expect(merged_hash[3]).to eq({"druid"=>"druid:aa111aa1111", "latest_change"=>"2014-12-20T21:03:32Z", "targets"=>["Searchwork", "Revs"], :type=>"index"})      
+      merged_hash = reader.merge_and_sort(JSON.parse(index_hash), JSON.parse(delete_hash))
+
+      expect(merged_hash.length).to eq(6)
+      expect(merged_hash[0]).to eq(:druid => 'druid:bb111bb1111', :latest_change => '2014-12-10T21:03:32Z', :type => 'delete')
+      expect(merged_hash[1]).to eq(:druid => 'druid:bb111bb1111', :latest_change => '2014-12-15T10:03:32Z', :target => 'Revs', :type => 'index')
+      expect(merged_hash[2]).to eq(:druid => 'druid:aa111aa1111', :latest_change => '2014-12-15T21:03:32Z', :type => 'delete')
+      expect(merged_hash[3]).to eq(:druid => 'druid:aa111aa1111', :latest_change => '2014-12-20T21:03:32Z', :target => 'Sw_stage', :type => 'delete')
+      expect(merged_hash[4]).to eq(:druid => 'druid:aa111aa1111', :latest_change => '2014-12-20T21:03:32Z', :target => 'Revs', :type => 'index')
+      expect(merged_hash[5]).to eq(:druid => 'druid:aa111aa1111', :latest_change => '2014-12-20T21:03:32Z', :target => 'Searchwork', :type => 'index')
     end
-    
-    it "should return an empty list for empty index and delete" do
-      reader = DiscoveryDispatcher::PurlFetcherReader.new("","")
-      merged_hash = reader.merge_and_sort({},{} )
+
+    it 'should return an empty list for empty index and delete' do
+      reader = DiscoveryDispatcher::PurlFetcherReader.new('', '')
+      merged_hash = reader.merge_and_sort({}, {})
       expect(merged_hash).to eq([])
     end
-    
-    it "should return a sorted list for  index hash with items and empty delete hash" do
-      index_hash= '{"changes":[ {"druid":"druid:aa111aa1111","latest_change":"2014-12-20T21:03:32Z","targets":["Searchwork","Revs"]},  {"druid":"druid:bb111bb1111","latest_change":"2014-12-15T10:03:32Z","targets":["Revs"]}]}'
-      reader = DiscoveryDispatcher::PurlFetcherReader.new("","")
-      merged_hash = reader.merge_and_sort(JSON.parse(index_hash),{} )
-      expect(merged_hash.length).to eq(2)
-      expect(merged_hash[0]).to eq({"druid"=>"druid:bb111bb1111", "latest_change"=>"2014-12-15T10:03:32Z", "targets"=>["Revs"], :type=>"index"} )
-      expect(merged_hash[1]).to eq({"druid"=>"druid:aa111aa1111", "latest_change"=>"2014-12-20T21:03:32Z", "targets"=>["Searchwork", "Revs"], :type=>"index"})      
-    end
-    
-    it "should return a sorted list for empty index hash and delete hash with items" do
-      delete_hash = '{"deletes":[{"druid":"druid:aa111aa1111","latest_change":"2014-12-15T21:03:32Z"},{"druid":"druid:bb111bb1111","latest_change":"2014-12-10T21:03:32Z"} ]}'
-      reader = DiscoveryDispatcher::PurlFetcherReader.new("","")
-      merged_hash = reader.merge_and_sort({}, JSON.parse(delete_hash)  )
-      expect(merged_hash.length).to eq(2)
-      expect(merged_hash[0]).to eq({"druid"=>"druid:bb111bb1111", "latest_change"=>"2014-12-10T21:03:32Z", :type=>"delete"})
-      expect(merged_hash[1]).to eq({"druid"=>"druid:aa111aa1111", "latest_change"=>"2014-12-15T21:03:32Z", :type=>"delete"} )      
-    end
-    
-  end
 
+    it 'should return a sorted list for  index hash with items and empty delete hash' do
+      index_hash = '{"changes":[ {"druid":"druid:aa111aa1111","latest_change":"2014-12-20T21:03:32Z","true_targets":["Searchwork","Revs"],"false_targets":["Sw_stage"]},  {"druid":"druid:bb111bb1111","latest_change":"2014-12-15T10:03:32Z","true_targets":["Revs"]}]}'
+      reader = DiscoveryDispatcher::PurlFetcherReader.new('', '')
+      merged_hash = reader.merge_and_sort(JSON.parse(index_hash), {})
+      expect(merged_hash.length).to eq(4)
+      expect(merged_hash[0]).to eq(:druid=>"druid:bb111bb1111", :latest_change=>"2014-12-15T10:03:32Z", :target=>"Revs", :type=>"index")
+      expect(merged_hash[1]).to eq(:druid=>"druid:aa111aa1111", :latest_change=>"2014-12-20T21:03:32Z", :target=>"Sw_stage", :type=>"delete")
+      expect(merged_hash[2]).to eq(:druid=>"druid:aa111aa1111", :latest_change=>"2014-12-20T21:03:32Z", :target=>"Revs", :type=>"index")
+      expect(merged_hash[3]).to eq(:druid=>"druid:aa111aa1111", :latest_change=>"2014-12-20T21:03:32Z", :target=>"Searchwork", :type=>"index")
+    end
+
+    it 'should return a sorted list for empty index hash and delete hash with items' do
+      delete_hash = '{"deletes":[{"druid":"druid:aa111aa1111","latest_change":"2014-12-15T21:03:32Z"},{"druid":"druid:bb111bb1111","latest_change":"2014-12-10T21:03:32Z"} ]}'
+      reader = DiscoveryDispatcher::PurlFetcherReader.new('', '')
+      merged_hash = reader.merge_and_sort({}, JSON.parse(delete_hash))
+      expect(merged_hash.length).to eq(2)
+      expect(merged_hash[0]).to eq(:druid=>"druid:bb111bb1111", :latest_change=>"2014-12-10T21:03:32Z", :type=>"delete")
+      expect(merged_hash[1]).to eq(:druid=>"druid:aa111aa1111", :latest_change=>"2014-12-15T21:03:32Z", :type=>"delete")
+    end
+  end
 end
