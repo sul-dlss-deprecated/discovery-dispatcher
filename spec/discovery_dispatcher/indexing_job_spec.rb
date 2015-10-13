17ae8dc7f20ce36af9686ed5c75ae028243e303d
Laney McGlohon <laneymcg@stanford.edu>
2015-10-12 17:19:32 -0700

    Replaced POST with PUT

--- spec/discovery_dispatcher/indexing_job_spec.rb
+++ spec/discovery_dispatcher/indexing_job_spec.rb
@@ -4,8 +4,8 @@ describe DiscoveryDispatcher::IndexingJob do
       VCR.use_cassette('index_xz404nk7341') do
         index_job = DiscoveryDispatcher::IndexingJob.new('index', 'xz404nk7341', 'target1')
         Rails.configuration.targets_url_hash = { 'target1' => { 'url' => 'http://localhost:3000' } }
-        expect_any_instance_of(DiscoveryDispatcher::IndexingJob).to receive('build_request_command').with('xz404nk7341', 'post', 'http://localhost:3000').and_return('RestClient.post "http://localhost:3000/items/xz404nk7341", ""')
-        expect_any_instance_of(DiscoveryDispatcher::IndexingJob).to receive('run_request_command').with('xz404nk7341', 'index', 'RestClient.post "http://localhost:3000/items/xz404nk7341", ""')
+        expect_any_instance_of(DiscoveryDispatcher::IndexingJob).to receive('build_request_command').with('xz404nk7341', 'put', 'http://localhost:3000').and_return('RestClient.put "http://localhost:3000/items/xz404nk7341", ""')
+        expect_any_instance_of(DiscoveryDispatcher::IndexingJob).to receive('run_request_command').with('xz404nk7341', 'index', 'RestClient.put "http://localhost:3000/items/xz404nk7341", ""')
         index_job.perform
       end
     end
@@ -27,9 +27,9 @@ describe DiscoveryDispatcher::IndexingJob do
   end
 
   describe '.get_method' do
-    it 'returns post for index action' do
+    it 'returns put for index action' do
       index_job = DiscoveryDispatcher::IndexingJob.new
-      expect(index_job.get_method('index', '')).to eq('post')
+      expect(index_job.get_method('index', '')).to eq('put')
     end
     it 'returns delete for delete action' do
       index_job = DiscoveryDispatcher::IndexingJob.new
@@ -45,31 +45,31 @@ describe DiscoveryDispatcher::IndexingJob do
     it 'raises an exception for not found purl with response 202' do
       VCR.use_cassette('index_xz404nk7341') do
         index_job = DiscoveryDispatcher::IndexingJob.new
-        command = 'RestClient.post "http://localhost:3000/items/xz404nk7341", ""'
+        command = 'RestClient.put "http://localhost:3000/items/xz404nk7341", ""'
         expect(index_job.run_request_command('xz404nk7341', 'index', command)).to be nil
       end
     end
     it 'raises an exception for not found purl with response 202' do
       VCR.use_cassette('index_bb003xz2306_nopurl') do
         index_job = DiscoveryDispatcher::IndexingJob.new
-        command = 'RestClient.post "http://localhost:3000/items/bb003xz2306", ""'
+        command = 'RestClient.put "http://localhost:3000/items/bb003xz2306", ""'
         expect { index_job.run_request_command('bb003xz2306', 'index', command) }.to raise_error(RuntimeError)
       end
     end
     it 'raises an exception with unfound host' do
       VCR.use_cassette('index_bb003xz2306_nohost') do
         index_job = DiscoveryDispatcher::IndexingJob.new
-        command = 'RestClient.post "http://target/items/bb003xz2306", ""'
+        command = 'RestClient.put "http://target/items/bb003xz2306", ""'
         expect { index_job.run_request_command('bb003xz2306', 'index', command) }.to raise_error(RuntimeError)
       end
     end
   end
 
   describe '.build_request_command' do
-    it 'returns a request command based on the valid input and post method ' do
+    it 'returns a request command based on the valid input and put method ' do
       index_job = DiscoveryDispatcher::IndexingJob.new
-      actual_command = index_job.build_request_command('ab123cd4567', 'post', 'http://target')
-      expect(actual_command).to eq('RestClient.post "http://target/items/ab123cd4567?subtargets%5B%5D=default", ""')
+      actual_command = index_job.build_request_command('ab123cd4567', 'put', 'http://target')
+      expect(actual_command).to eq('RestClient.put "http://target/items/ab123cd4567?subtargets%5B%5D=default", ""')
     end
 
     it 'returns a request command based on the valid input and delete method ' do
