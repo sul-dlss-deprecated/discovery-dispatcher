5e8f0cc36618e458d64e5eef1a6868b13e6a6061
Laney McGlohon <laneymcg@stanford.edu>
2016-07-13 11:55:43 -0700

    Remove eval call for running rest client command

--- lib/discovery_dispatcher/indexing_job.rb
+++ lib/discovery_dispatcher/indexing_job.rb
@@ -10,42 +10,47 @@ module DiscoveryDispatcher
       target_url  = get_target_url target, druid
       method      = get_method type, druid
 
-      request_command = build_request_command(druid, method, target_url, target)
-      run_request_command(druid, type, request_command)
+      url = build_request_url(druid, target_url, target)
+      run_request(druid, type, url)
 
       # Request went successfully
       Rails.logger.debug "Completing #{druid_id} for target #{target}"
     end
 
     # @param druid [String] represents the druid on the form of ab123cd4567
-    # @param method [String] it may be put or delete
     # @param target_url [String] the url for the indexing service
     # @param solr_target [Array] solr target for solr core
     # @return [String] RestClient request command
-    def build_request_command(druid, method, target_url, solr_target)
+    def build_request_url(druid, target_url, solr_target)
       solr_target = Array(solr_target)
-      request_command = "RestClient.#{method} \"#{target_url}/items/#{druid}?#{solr_target.to_query('solr_target')}\""
-      request_command = "#{request_command}, \"\"" if method == 'put'
-      request_command
+      "#{target_url}/items/#{druid}?#{solr_target.to_query('solr_target')}"
     end
 
     # It runs the request command
     # @param druid [String] represents the druid on the form of ab123cd4567
     # @param type [String] index or delete
     # @param request_command [String] RestClient request command
-    def run_request_command(druid, type, request_command)
+    def run_request(druid, type, request_url)
       response = nil
       begin
-        response = eval(request_command)
+        conn = Faraday.new(url: request_url)
+        if type == 'index'
+          response = conn.put do |request|
+            request.body = ''
+          end
+        else
+          response = conn.delete
+        end
+        response
       rescue => e # An exception happened during the request
-        raise "#{type} #{druid} with #{request_command} has an error.\n#{e.inspect}\n#{e.message}\n#{e.backtrace}"
+        raise "#{type} #{druid} with #{request_url} has an error.\n#{e.inspect}\n#{e.message}\n#{e.backtrace}"
       end
 
       # The request doesn't raise an exception but it doesn't return data
-      raise "#{type} #{druid} with #{request_command} has an error." if response.nil?
+      fail "#{type} #{druid} with #{request_url} has an error." if response.nil?
 
       # The request return with anything other than 200, so it is a problem
-      raise "#{type} #{druid} with #{request_command} has an error.\n#{response.code}\n\n#{response.inspect}" if response.code != 200
+      fail "#{type} #{druid} with #{request_url} has an error.\n#{response.status}\n\n#{response.inspect}" if response.status != 200
     end
 
     # It gets the indexing service url based on the target name
