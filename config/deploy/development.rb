9d31bf0725ca0589aa380152d34e5ea29a0c3b79
Erin Fahy <efahy@stanford.edu>
2016-07-15 18:00:20 -0700

    Updates capistano files & remove squash from deploy process

--- config/deploy/development.rb
+++
@@ -1,4 +0,0 @@
-set :bundle_without, %w(deployment test).join(' ')
-set :rails_env, 'development'
-
-Capistrano::OneTimeKey.generate_one_time_key!
---
+++ config/deploy/stage.rb
@@ -0,0 +1,5 @@
+server 'discovery-dispatcher-stage.stanford.edu', user: fetch(:user), roles: %w{web db app}
+set :bundle_without, %w(sqlite test development deployment).join(' ')
+
+Capistrano::OneTimeKey.generate_one_time_key!
+set :rails_env, 'production'
