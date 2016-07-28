12d61b5ff453009a88082900bd9044a4d898c5d1
Peter Mangiafico <peter@mangiafico.org>
2016-07-28 10:08:46 -0700

    use strong parameters in items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,20 @@
 class ItemsController < ApplicationController
   respond_to :json
   def new
-    type = params[:type]
-    druid = params[:druid]
-    targets = params[:targets]
-    raise 'missing required params' unless type && druid && targets
-    record = { type: type, druid: druid, targets: targets }
-    DiscoveryDispatcher::IndexingJobManager.enqueue_process_record record
-    @status = "#{druid} is enqueued successfully into the process queue"
+    DiscoveryDispatcher::IndexingJobManager.enqueue_process_record record_params
+    @status = "#{record_params[:druid]} is enqueued successfully into the process queue"
     render json: @status.to_json
   rescue => e
     Rails.logger.error { "Items controller failed to add a new item to the queue\n#{e.message}" }
     render json: e.message, :status=>500
   end
+
+  private
+  def record_params
+    params.require(:type)
+    params.require(:druid)
+    params.require(:targets)
+    params.slice(:type, :druid, :targets)
+  end
+
 end
